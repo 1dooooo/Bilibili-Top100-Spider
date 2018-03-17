470a6509b842c1975e09c81c960fc1e6912b37c1
ZhouZihao <zhouzihao2008@126.com>
2018-03-17 17:44:57 +0800

    v1.2 Update It's a test version. Some bugs repaired Use ajax instead of selenium However,you can't use some part of it(I will fix it soon)

--- bilibili.py
+++ bilibili.py
@@ -1,32 +1,26 @@
 # -*- coding=utf-8 -*-
 
 from pyquery import PyQuery
-from selenium import webdriver
-import re
 import json
 import requests
 
 
 def parse(url):
-    browser = webdriver.Chrome()
-    browser.get(url)
-    html = browser.page_source
-    doc = PyQuery(html)
-    items = doc('.rank-item').items()
-    fp = open('data.json', 'w', encoding='utf-8')
-    fp.close()
+    response = requests.get(url)
+    items = response.json().get("rank").get("list")
+    i = 1
     for item in items:
         data = {
-            '排名': item.find('.num').text(),
-            '名称': item.find('.title').text(),
-            '播放量': item.find('.play').text(),
-            '弹幕': item.find('.dm').text(),
-            'up主': item.find('.author').text(),
-            '综合评分': re.match('\d*', item.find('.pts').text()).group(),
+            '排名': i,
+            '名称': item.get("title"),
+            '播放量': item.get("play"),
+            '弹幕': item.get("video_review"),
+            'up主': item.get("author"),
+            '综合评分': item.get("pts"),
         }
+        i = i + 1
         with open('data.json', 'a', encoding='utf-8') as file:
             file.write(json.dumps(data, indent=2, ensure_ascii=False))
-    browser.close()
     print("Succeed!")
 
 
@@ -47,7 +41,13 @@ def select1():
 
 
 def select2(typ):
+    # 由于B站番剧榜和影视榜的URL与其他不同，所以特殊处理
+    if typ == "bangumi":
+        typ = "bangumi/13/0/3/"
+    if typ == "cinema":
+        typ = "cinema/177/0/3/"
     url = "https://www.bilibili.com/ranking#!/%s" % typ
+    print(url)
     html = requests.get(url).text
     doc = PyQuery(html)
     items = doc(".rank-tab li").items()
@@ -94,37 +94,51 @@ def select4():
         yield thing
 
 
-def make_url(typ,tid,typ2,rang):
-    return "https://www.bilibili.com/ranking#!/%s/%s/%s/%s/" % (typ,tid,typ2,rang)
+def make_url(typ,rang,tid,newv):
+    # 由于影视榜的ajax用的名称是all而不是cinema，所以特殊处理
+    if typ == "cinema":
+        typ = "all"
+    if newv == "1":
+        return "https://www.bilibili.com/index/rank/%s-0%s-%s.json" % (typ,rang,tid)
+    else:
+        return "https://www.bilibili.com/index/rank/%s-%s-%s.json" % (typ,rang,tid)
 
 
 li1 = []
 li2 = []
 li3 = []
 li4 = []
+open('data.json', 'w', encoding='utf-8').close()
+
 
 for things in select1():
     li1.append(things)
     print(things["id"],things["txt"])
-menu = int(input("输入代号选择榜单："))
+typ = int(input("输入代号选择榜单："))
+typ = li1[typ]["type"]
 print("\n")
 
-for things in select2(li1[menu]["type"]):
+for things in select2(typ):
     li2.append(things)
     print(things["id"],things["txt"])
 tab = int(input("输入代号选择标签："))
+tab = li2[tab]["tid"]
 print("\n")
 
 for things in select3():
     li3.append(things)
     print(things["id"],things["txt"])
 newv = int(input("输入代号选择检索投稿："))
+newv = li3[newv]["type"]
 print("\n")
 
 for things in select4():
     li4.append(things)
     print(things["id"],things["txt"])
 rang = int(input("输入代号选择检索时长："))
+rang = li4[rang]["range"]
 print("\n")
 
-parse(make_url(li1[menu]["type"],li2[tab]["tid"],li3[newv]["type"],li4[rang]["range"]))
+url = make_url(typ,rang,tab,newv)
+print(url)
+parse(url)
